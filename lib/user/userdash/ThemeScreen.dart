0419dc434c3f1cd973781dfaba7779139f98c640
KaranUderani <karanuderani60776@gmail.com>
2025-04-04 14:50:41 +0530

    ui chng

--- lib/user/userdash/ThemeScreen.dart
+++ lib/user/userdash/ThemeScreen.dart
@@ -10,7 +10,7 @@ class ThemeScreen extends StatelessWidget {
       ),
       body: Center(
         child: Text(
-          "Coming Soon...",
+          "Coming Soon...vedya",
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.grey),
         ),
       ),
